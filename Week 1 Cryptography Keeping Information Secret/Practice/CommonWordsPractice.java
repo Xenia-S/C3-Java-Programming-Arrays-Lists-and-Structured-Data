df31e963bb15b7e2e23bab25e9b0b6d387e6ee7a
Xenia Shatova <98309459+Xenia-S@users.noreply.github.com>
2022-09-27 01:25:50 +0600

    Update CommonWordsPractice.java

--- Week 1 Cryptography Keeping Information Secret/Practice/CommonWordsPractice.java
+++ Week 1 Cryptography Keeping Information Secret/Practice/CommonWordsPractice.java
@@ -1,7 +1,7 @@
 import edu.duke.*;
 
 public class CommonWordsPractice {
-    public String[] getCommon() {            // делаем из файла массив
+    public String[] getCommon() { 
         FileResource fr = new FileResource("data/common.txt");
         String[] common = new String[20];
         int i = 0;
@@ -12,8 +12,8 @@ public class CommonWordsPractice {
         return common;
     }
     
-    public int[] countWords(FileResource fr, String[] common, int[] count) {   // считаем совпадения слов, записываем в массив такого же размера, как common
-        for (String word : fr.words()) {                                       // для MyWay нужно убирать int[] count из параметров
+    public int[] countWords(FileResource fr, String[] common, int[] count) {
+        for (String word : fr.words()) {
             word = word.toLowerCase();
             for (int k=0; k<common.length; k++) {
                 if (word.equals(common[k])) {
@@ -37,25 +37,5 @@ public class CommonWordsPractice {
         for (int k=0; k<common.length; k++){
                 System.out.println(common[k] + "\t" + totalCount[k]);
         }
-    }
-    
-    /*public void countShakespeareMyWay() {    // мой вариант стичает common отдельно в каждой play
-        String[] plays = {"caesar.txt", "common.txt", "errors.txt", "hamlet.txt", "likeit.txt", "macbeth.txt", "romeo.txt", "small.txt"};
-        String[] common = getCommon();
-        int[] totalCount = new int[common.length];
-        for (int i=0; i<plays.length; i++) {
-            FileResource fr = new FileResource("data/"+plays[i]);
-            int[] count = countWords(fr, common);
-            for (int k=0; k<common.length; k++){
-                System.out.println(common[k] + "\t" + count[k]);
-                totalCount[k] = totalCount[k] + count[k];
-            }
-            System.out.println("Done with "+ plays[i]);
-        }
-        System.out.println("\n TOTAL:");
-        for (int k=0; k<common.length; k++){
-                System.out.println(common[k] + "\t" + totalCount[k]);
-        }
-    }*/
-    
+    }    
 }
